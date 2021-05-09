62ea1f687e3f43c038030b89e2390abef79dcece
root <root@localhost.localdomain>
2021-05-09 21:00:08 +0800

    OK

--- bibly-2021-00/Main.java
+++ bibly-2021-00/Main.java
@@ -4,10 +4,53 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 
+class Verse {
+	int vnum;
+	String vers;
+	Verse(int vnum,String vers) {
+		this.vnum=vnum;
+		this.vers=vers;
+	}
+}
 
-class Main {
+class Chapter {
+	int cnum;
+	ArrayList<Verse> verses = new ArrayList<>();
+	Chapter(int cnum) {
+		this.cnum=cnum;
+	}
+}
+
+class Book {
+	String bname;
+	int bnum;
+	ArrayList<Chapter> chapters = new ArrayList<>();
+	Book(String bname,int bnum) {
+		this.bname=bname;
+		this.bnum=bnum;
+	}
+}
+
+class Bible {
+	ArrayList<Book> books = new ArrayList<>();
+}
+
+
+
+public class Main {
+
+	enum State {
+		DEFAULT,
+		LETTER,
+		BOOK,
+		CHAPTER,
+		START_VERSE,
+		END_VERSE,
+		PRINT
+	}
 
 	static String version = "bibly 0.1";
+
 	static String currentBibleVersion = "kjv.csv";
 
   static Scanner reader = null;
@@ -15,9 +58,11 @@ class Main {
 
 	static String cmd = "";
 
+	static String prompt = "bibly> ";
 
 
-	static void cls() {
+
+	static void clear() {
     System.out.println("\33[2J\33[0;0H");
 	}
 
@@ -40,11 +85,14 @@ class Main {
 		println("command\t->\taction");
 		println(".help\t->\tshow help");
 		println(".books\t->\tshow books");
-		println(".find\t->\tshow books per chapter");
+		println(".read\t->\tshow books per chapter");
 		println(".search\t->\tshow search");
-		println(".end\t->\texit");
+		println(".seek\t->\tshow seek");
+		println(".clear\t->\tclear screen");
+		println(".exit\t->\texit");
 		println("");
 	}
+
 
 
 	static void openBibleFile(String bibleVersion) {
@@ -117,7 +165,10 @@ class Main {
 
 
 	static void printSearch() {
-		print("Search: ");    String textToSearch=input.nextLine();
+		print("Search: ");    
+		String textToSearch=input.nextLine().trim();
+
+		if(textToSearch.isEmpty()) return;
 
 		String line="";
 		String[] arr1;
@@ -144,34 +195,99 @@ class Main {
 	}
 
 
+
+	static Bible parseBible() {		
+		Bible bible = new Bible();
+
+		String line="";
+		String[] arr1;
+		String[] arr2;
+
+		int bnum=0;
+		int cnum=0;
+		int vnum=0;
+		String bname="";
+		String vers="";
+		boolean start=true;
+
+		openBibleFile(currentBibleVersion);
+
+		while(reader.hasNextLine()) {
+			line=reader.nextLine();
+			arr1=line.split("\\|");
+			arr2=arr1[1].split("\\:");
+
+			if(!arr1[0].equals(bname)) {
+				if(start) start=false; else bnum++;
+				bname = arr1[0]; 
+			} 
+
+			try {
+				cnum = Integer.parseInt(arr2[0])-1; 
+			} catch(NumberFormatException e) {
+				println("Invalid Chapter Number");
+				System.exit(0);
+			}
+
+			try {
+				vnum = Integer.parseInt(arr2[1])-1; 
+			} catch(NumberFormatException e) {
+				println("Invalid Verse Number");
+				System.exit(0);
+			}
+
+			vers = arr1[2]; 
+			
+//			println((bnum+1)+" "+bname+" "+(cnum+1)+":"+(vnum+1));
+
+			bible.books.add(new Book(bname,bnum));
+			bible.books.get(bnum).chapters.add(new Chapter(cnum));
+			bible.books.get(bnum).chapters.get(cnum).verses.add(new Verse(vnum,vers));
+						
+		}
+		closeBibleFile();
+
+		return bible;
+	}
+
+
+
   public static void main(String[] args) {
+
+		Bible bible = new Bible();
 
 		boolean finish=false;
 
 		ArrayList<String> bibleBooks;
 
-		cls();
+		clear();
 
 		print(version+"\n\n");
 
 		printHelp();
 
+		bible=parseBible();
+
 		while(!finish) {
 			
-			print("> ");
+			print(prompt);
 			
 			cmd=input.nextLine();
 			
-			if(cmd.equals(".end")) {
+			if(cmd.equals(".exit")) {
 				finish=true;
+			} else if(cmd.equals(".clear")) {
+				clear();
 			} else if(cmd.equals(".help")) {
 				printHelp();
 			} else if(cmd.equals(".books")) {
 				printBibleBooks();
-			} else if(cmd.equals(".find")) {
+			} else if(cmd.equals(".read")) {
 				printBibleBooksPerChapter();
 			} else if(cmd.equals(".search")) {
 				printSearch();
+			} else {				
+				println("Invalid command.");
 			}
 
 		}
